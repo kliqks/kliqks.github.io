c9da139ab1f1601d33a9010478977016c259138e
kliqks <talon5@email.phoenix.edu>
2016-02-20 17:49:12 +0900

    sdfsfsf

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -26,3 +26,17 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
        window.location.hash = hash;
      });
 });
+$("#back-to-top a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+   // store hash
+   var hash = this.hash;
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 700, function(){
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
